1ef03a9a0984c70c08cf44a9f9f687129eb7319f
Micah R Ledbetter <me@micahrl.com>
2021-01-22 02:06:20 -0600

    Show VERCEL_ENV, and change format of dev URL to match what Vercel shows

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,10 +3,12 @@
 export default (req, res) => {
   const hostFromVercelEnv = process.env.VERCEL_URL || "unset"
   const hostFromDevEnv = process.env.LOCAL_DEV_URL || "unset"
+  const vercelEnv = process.env.VERCEL_ENV || "unset"
   res.statusCode = 200
   res.json({
     hostFromHeaders: req.headers.host,
     hostFromVercelEnv,
     hostFromDevEnv,
+    vercelEnv,
   })
 }
