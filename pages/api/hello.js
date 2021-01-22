11795bea7daa40356b8b30c16d6828bc87c99eb0
Micah R Ledbetter <me@micahrl.com>
2021-01-22 01:54:27 -0600

    Initial commit
    Use port 2999 for local dev
    
    Includes changes to pages/api/hello.js

---
+++ pages/api/hello.js
@@ -0,0 +1,12 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default (req, res) => {
+  const hostFromVercelEnv = process.env.VERCEL_URL || "unset"
+  const hostFromDevEnv = process.env.LOCAL_DEV_URL || "unset"
+  res.statusCode = 200
+  res.json({
+    hostFromHeaders: req.headers.host,
+    hostFromVercelEnv,
+    hostFromDevEnv,
+  })
+}
